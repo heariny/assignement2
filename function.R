9431163d7157709262b5140c0ed0d641ca7c2aab
myang <heariny@gmail.com>
2017-10-10 23:46:32 +0200

    exercice3 too much,can't finish in one night X(

--- function.R
+++ function.R
@@ -36,7 +36,24 @@ sum_divided_by<-function(vector,number){
   }
 }
 
-#----------------3 function-----------------------#
+#----------------4 function-----------------------#
+grouped_violin_plot <- function(d, var, grouping_var) {
+  # Create the base ggplot object
+  p <- ggplot2::ggplot(d, ggplot2::aes_string(y=var,
+                                              x=grouping_var,
+                                              fill=grouping_var))+ ggplot2::geom_violin() 
+  # YOUR CODE HERE: Create a violin plot
+  return(p)
+}
+
+#----------------5 function-----------------------#
+grouped_violin_plot <- function(d, var, grouping_var) {
+  # Create the base ggplot object
+  p <- ggplot2::ggplot(d, ggplot2::aes_string(y=var,
+                                              x=grouping_var,
+                                              fill=grouping_var))+ ggplot2::geom_violin() }
+
+#----------------6 function-----------------------#
 my_mean<-function(vector){
   if (!is.numeric(vector)){#verify the argument type
     return (NULL)
@@ -46,4 +63,46 @@ my_mean<-function(vector){
                        #so we need the number of elements,which is just the length of vector
     return(sum_divided_by(vector,len))
   }
+}
+
+#----------------7 function-----------------------#
+difference_in_medians <- function(d, var, grouping_var, group1, group2) {
+  d_1 <- dplyr::filter(d, get(grouping_var) == group1)
+  d_2 <- dplyr::filter(d, get(grouping_var) == group2)
+  result<-median(d_1[,c(var)])-median(d_2[,c(var)])
+  
+  return(result)
+}
+
+#----------------8 function-----------------------#
+randomize <- function(d, var) {
+  n<-nrow(d)
+  d[[var]] <- sample(d[,c(var)],n)
+  return(d)}
+
+#----------------9 function-----------------------#
+permutation_twogroups <- function(d, var, grouping_var, group1, group2, statistic,
+                                  n_samples=9999) {
+  observed_statistic <- statistic(d, var, grouping_var, group1, group2)
+  permutation_statistics <- rep(0, n_samples)
+  for (i in 1:n_samples) {
+    # YOUR CODE HERE: use randomize(...) to create a permutation and then
+    # fill in the vector permutation_statistics with the
+    # value of statistic(...) for this new permutation
+    d_fake<-randomize(d,var)
+    d_1_fake<-dplyr::filter(d_fake, get(grouping_var)  == group1)
+    d_2_fake<-dplyr::filter(d_fake, get(grouping_var)  == group2)
+    permutation_statistics[i] <- median(d_1_fake[,c(var)]) - median(d_2_fake[,c(var)])
+  }
+  result <- list(observed=observed_statistic,
+                 permuted=permutation_statistics)
+  return(result)
+}
+
+#---------------10function----------------------------#
+new_test_statistic<-function(d, var, grouping_var, group1, group2){
+  d_1 <- dplyr::filter(d, get(grouping_var) == group1)
+  d_2 <- dplyr::filter(d, get(grouping_var) == group2)
+  result<-0.1
+  return(result)
 }
